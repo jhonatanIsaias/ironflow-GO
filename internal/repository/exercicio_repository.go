249bb31d6b0ae47e225a6fcac18f9d680c7e0236
kaylaineasb <kaylaineasb9@gmail.com>
2026-06-10 15:18:06 -0300

    fix: cast do uuid

--- internal/repository/exercicio_repository.go
+++ internal/repository/exercicio_repository.go
@@ -3,11 +3,11 @@ package repository
 import (
 	"context"
 	"errors"
+
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
-	
-	
-	"ironflow/internal/model" 
+
+	"ironflow/internal/model"
 )
 
 type ExercicioRepository struct {
@@ -18,32 +18,31 @@ func NovoExercicioRepository(db *pgxpool.Pool) *ExercicioRepository {
 	return &ExercicioRepository{DB: db}
 }
 
-
-func (r *ExercicioRepository) Salvar (ctx context.Context, e *model.Exercicio, usuTxID string) error{
+func (r *ExercicioRepository) Salvar(ctx context.Context, e *model.Exercicio, usuTxID string) error {
 	sql := `
 		INSERT INTO treino.exe_exercicio (exe_tx_nome, exe_tx_grupo_muscular, exe_tx_grupo_muscular_sinegista, exe_tx_tipo_equipamento,usu_tx_id)
 		VALUES ($1, $2, $3, $4, $5) RETURNING exe_nr_id, created_at, updated_at`
 
-		err := r.DB.QueryRow(ctx, sql,
+	err := r.DB.QueryRow(ctx, sql,
 		e.ExeTxNome,
 		e.ExeTxGrupoMuscular,
 		e.ExeTxGrupoMuscularSinergista,
 		e.ExeTxTipoEquipamento,
 		usuTxID,
 	).Scan(
-		   &e.ExeNrID,
-		   &e.CreatedAt,
-		   &e.UpdatedAt,
-		)
+		&e.ExeNrID,
+		&e.CreatedAt,
+		&e.UpdatedAt,
+	)
 
-	return err;
+	return err
 
 }
 
-func (r *ExercicioRepository) Editar(ctx context.Context, e *model.Exercicio,usuTxID string) error {
-	
-	sql := 
-	`
+func (r *ExercicioRepository) Editar(ctx context.Context, e *model.Exercicio, usuTxID string) error {
+
+	sql :=
+		`
 		UPDATE treino.exe_exercicio
 		SET exe_tx_nome = $1, 
 		exe_tx_grupo_muscular = $2, 
@@ -53,7 +52,7 @@ func (r *ExercicioRepository) Editar(ctx context.Context, e *model.Exercicio,usu
 		WHERE exe_nr_id = $5 AND deleted_at IS NULL AND usu_tx_id = $6
 		RETURNING created_at, updated_at
 	`
-	 err := r.DB.QueryRow(ctx, sql,
+	err := r.DB.QueryRow(ctx, sql,
 		e.ExeTxNome,
 		e.ExeTxGrupoMuscular,
 		e.ExeTxGrupoMuscularSinergista,
@@ -61,10 +60,10 @@ func (r *ExercicioRepository) Editar(ctx context.Context, e *model.Exercicio,usu
 		e.ExeNrID,
 		usuTxID,
 	).
-	Scan(
-		&e.CreatedAt,
-		&e.UpdatedAt,
-	)
+		Scan(
+			&e.CreatedAt,
+			&e.UpdatedAt,
+		)
 
 	if errors.Is(err, pgx.ErrNoRows) {
 		return errors.New("Não é possível editar: Exercício inexistente")
@@ -77,29 +76,28 @@ func (r *ExercicioRepository) Editar(ctx context.Context, e *model.Exercicio,usu
 	return nil
 }
 
-func (r *ExercicioRepository) BuscarTodos(ctx context.Context,usuTxID string)([]model.Exercicio,error){
+func (r *ExercicioRepository) BuscarTodos(ctx context.Context, usuTxID string) ([]model.Exercicio, error) {
 	sql := `
-		SELECT exe_nr_id, exe_tx_nome, usu_tx_id, exe_tx_grupo_muscular, exe_tx_grupo_muscular_sinegista, exe_tx_tipo_equipamento, created_at, updated_at
-		FROM treino.exe_exercicio
-		WHERE (usu_tx_id IS NULL OR usu_Tx_id = $1 ) AND deleted_at IS NULL
-	`
+        SELECT exe_nr_id, exe_tx_nome, exe_tx_grupo_muscular, usu_tx_id, exe_tx_grupo_muscular_sinegista, exe_tx_tipo_equipamento, created_at, updated_at
+        FROM treino.exe_exercicio
+        WHERE (usu_tx_id IS NULL OR usu_tx_id = $1::UUID) AND deleted_at IS NULL
+    `
 
 	rows, err := r.DB.Query(ctx, sql, usuTxID)
 	if err != nil {
 		return nil, err
 	}
-
 	defer rows.Close()
 
 	var exercicios []model.Exercicio
 	for rows.Next() {
 		var e model.Exercicio
-	
+
 		err := rows.Scan(
 			&e.ExeNrID,
 			&e.ExeTxNome,
-			
 			&e.ExeTxGrupoMuscular,
+			&e.UsuTxID,
 			&e.ExeTxGrupoMuscularSinergista,
 			&e.ExeTxTipoEquipamento,
 			&e.CreatedAt,
@@ -108,6 +106,7 @@ func (r *ExercicioRepository) BuscarTodos(ctx context.Context,usuTxID string)([]
 		if err != nil {
 			return nil, err
 		}
+
 		e.IsCustom = e.UsuTxID != nil
 		exercicios = append(exercicios, e)
 	}
@@ -127,7 +126,7 @@ func (r *ExercicioRepository) BuscarPorID(ctx context.Context, exeNrId int, usuT
 	`
 	var e model.Exercicio
 
-	err := r.DB.QueryRow(ctx, sql, exeNrId,usuTxID).Scan(
+	err := r.DB.QueryRow(ctx, sql, exeNrId, usuTxID).Scan(
 		&e.ExeNrID,
 		&e.ExeTxNome,
 		&e.ExeTxGrupoMuscular,
@@ -140,17 +139,16 @@ func (r *ExercicioRepository) BuscarPorID(ctx context.Context, exeNrId int, usuT
 	if errors.Is(err, pgx.ErrNoRows) {
 		return nil, errors.New("Exercício não encontrado")
 	} else if err != nil {
-		return nil, err 
+		return nil, err
 	}
 
 	return &e, nil
 }
 
-
 func (r *ExercicioRepository) Deletar(ctx context.Context, id int, usuTxID string) error {
-	
+
 	sql := `UPDATE treino.exe_exercicio SET deleted_at = NOW() WHERE exe_nr_id = $1 AND usu_tx_id = $2`
-	
+
 	comando, err := r.DB.Exec(ctx, sql, id, usuTxID)
 	if err != nil {
 		return err
